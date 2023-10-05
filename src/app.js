593035d6e67ca0a530685c1ed5ccdab51df504a3
Shawky Khalil <shawkykhalil@gmail.com>
2023-10-06 02:08:51 +0300

    Disabled development links in CORS

--- src/app.js
+++ src/app.js
@@ -29,8 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5174",
-      "http://127.0.0.1:5173",
+      // "http://localhost:5174",
+      // "http://localhost:5173",
+      // "http://127.0.0.1:5174",
+      // "http://127.0.0.1:5173",
       "https://ecommerce-client-react-seven.vercel.app",
     ],
     credentials: true,
